ae0c63559ec36d9e3c06ae0008460a8fbf11aa41
npret <nixiepret@gmail.com>
2024-12-13 17:21:29 +0100

    Added workaround for model file being too big
    model will now download from external link

--- app.py
+++ app.py
@@ -202,5 +202,6 @@ class PropertyApp:
 
 # Run the app
 if __name__ == "__main__":
-    app = PropertyApp("model/trained_model.pkl")
+    MODEL_URL = "https://drive.google.com/uc?id=1NJjCR_QQAtdWQ77YN815223f0ynm3-AG"
+    app = PropertyApp("MODEL_URL")
     app.run()
